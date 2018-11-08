fcfacafa3437946326cf52a03eefcef0cdb4d3a2
Dolores <alyciahathaways@gmail.com>
2018-11-09 00:37:01 +0800

    添加 gitignore

--- index.js
+++
@@ -1,4 +0,0 @@
-
-let another = require('./test.js')
-
-console.log(another)
--- test.js
+++
@@ -1,8 +0,0 @@
-// exports 是 module.exports 的简写
-// exports.test = 'Hello World'
-
-let test = 'Hello World'
-
-module.exports = {
-    string: test
-}
