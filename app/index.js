dd0e9c490700e899c9af9788941f2f0fffc18ef2
Frank <alyciahathaways@gmail.com>
2018-11-09 18:41:16 +0800

    分发 url

--- app/index.js
+++ app/index.js
@@ -8,14 +8,23 @@ class APP {
 
     }
     // 高阶函数
-    initServer(request, response) {
+    initServer() {
         // 初始化的工作
 
         return (request, response)=> {
-            // 每个请求进来的核心逻辑
-            fs.readFile('./public/index.html', 'utf8', (error, data)=> {
-                response.end(data)
-            })
+            // 每个请求进来的核心逻辑，根据 url 进行代码分发
+            let { url } = request
+            let staticFunction = (url)=> {
+                if (url === '/') {
+                    url = '/index.html'
+                }
+                fs.readFile(`./public${url}`, 'utf8', (error, data)=> {
+                    response.end(data)
+                })
+            }
+
+            staticFunction(url)
+            
         }
             
     }
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+/*
+ * created by Dolores
+ * 18/11/09
+ */
+
 const http = require('http')
 const PORT = 7000
 const APP = require('./app')
 const server = new APP()
 
-http.createServer(server.initServer).listen(PORT, ()=> {
+http.createServer(server.initServer()).listen(PORT, ()=> {
     console.log(`server listening on port ${PORT}`)
 })
