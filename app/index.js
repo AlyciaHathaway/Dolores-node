37522c70c934dfe63e880540ed22277ab6d275d2
Dolores <alyciahathaways@gmail.com>
2018-11-11 23:14:37 +0800

    如果是图片，用二进制编码的格式

--- app/index.js
+++ app/index.js
@@ -14,12 +14,21 @@ class APP {
         return (request, response)=> {
             // 每个请求进来的核心逻辑，根据 url 进行代码分发
             let { url } = request
+            // express 框架 app.use(static('public'))
+            // 相对路径改成绝对路径
+            let getPath = (url)=> {
+                return path.resolve(process.pwd(), 'public', `${url}`)
+            }
             let staticFunction = (url)=> {
                 if (url === '/') {
                     url = '/index.html'
                 }
-                fs.readFile(`./public${url}`, 'utf8', (error, data)=> {
-                    response.end(data)
+                let _path = getPath(url)
+                fs.readFile(_path, 'binary', (error, data)=> {
+                    if (error) {
+                        data = 'NOT FOUND'
+                    }
+                    response.end(data, 'binary')
                 })
             }
 
